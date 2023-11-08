a7b553f900df77e1c9d7b6a311c3d2d09be663fc
patriciafraga <patricia.f.fraga@gmail.com>
2023-11-08 20:11:01 -0300

    alteração host e rota

--- src/connection/connection.js
+++ src/connection/connection.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const knex = require("knex")({
   client: "pg",
   connection: {
-    host: process.env.DB_HOST || "localhost",
+    host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
