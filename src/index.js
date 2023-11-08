2604bb70c077470a17ffd537ccd35fcace2e81b4
patriciafraga <patricia.f.fraga@gmail.com>
2023-11-08 18:59:37 -0300

    inclusão cors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const cors = require('cors');
 const route = require("./rotas/route");
 
 app.use(route);
+app.use(cors());
 app.use(express.json());
 
 const port = process.env.PORT || 3000;
