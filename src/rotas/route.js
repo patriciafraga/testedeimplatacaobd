37b93369aa2530a06d84e34dcfd84893e6105e5c
patriciafraga <patricia.f.fraga@gmail.com>
2023-11-08 19:04:24 -0300

    alterção de rota

--- src/rotas/route.js
+++ src/rotas/route.js
@@ -3,10 +3,10 @@ const route = express();
 const knex = require('../connection/connection');
 route.use(express.json());
 
+// route.get('/', async (req,res) => {
+//     return res.status(200).json(`Deploy ok!`)
+// });
 route.get('/', async (req,res) => {
-    return res.status(200).json(`Deploy ok!`)
-});
-route.get('/carros', async (req,res) => {
     try {
         const listarCarros = await knex(`carros`);
     return res.status(200).json(listarCarros);
