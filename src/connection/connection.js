8990b4a2ada2429c9f365617290a388883073df6
patriciafraga <patricia.f.fraga@gmail.com>
2023-11-08 18:43:28 -0300

    alteração de conexão

--- src/connection/connection.js
+++ src/connection/connection.js
@@ -1,11 +1,15 @@
-const {Pool} = require('pg');
-require('dotenv').config();
-
-const pool = new Pool ({
-    host: process.env.DB_HOST || 'localhost',
+const { Pool } = require("pg");
+require("dotenv").config();
+const knex = require("knex")({
+  client: "pg",
+  connection: {
+    host: process.env.DB_HOST || "localhost",
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT || 5432
+    port: process.env.DB_PORT || 5432,
+    ssl: { rejectUnauthorized: false }
+  },
 });
-module.exports = pool;
+
+module.exports = knex;
--- src/rotas/route.js
+++ src/rotas/route.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const route = express();
-const pool = require('../connection/connection');
+const knex = require('../connection/connection');
 route.use(express.json());
 
 route.get('/', async (req,res) => {
     return res.status(200).json(`Deploy ok!`)
 });
 route.get('/carros', async (req,res) => {
-    const listarCarros = await pool.query(`select * from carros`);
+    const listarCarros = await knex(`carros`);
     return res.status(200).json(listarCarros.rows);
 });
 module.exports = route;
